de243adc9dfc0baabfa3e6e641cac67e54f7a035
윤지원 <dusqha6731@naver.com>
2025-01-31 12:39:53 +0900

    [FE] fix: 머지 후 오류 수정
    코드 일부 띄어쓰기 등 수정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -14,17 +14,16 @@ function App(): JSX.Element {
     // 추후 className에 touch-none overflow-hidden 설정시 스크롤이 방지됩니다
     <div className="fixed inset-0 overflow-auto">
       <Routes>
-      <Route path="/" element={<Landing />} >
-          <Route path="home" element={<Home />} />
-          <Route path="drawing" element={<Drawing />} />
-          <Route path="story/*" element={<Story />} />
-          <Route path="house" element={<MyHouse />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="friends" element={<Friends />} />
-          <Route path="parent" element={<Parent />} />
-          <Route path="test" element={<Test />} />
-      </Route>
-    </Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/drawing" element={<Drawing />} />
+        <Route path="/story/*" element={<Story />} />
+        <Route path="/house" element={<MyHouse />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/parent" element={<Parent />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
     </div>
   );
 }
