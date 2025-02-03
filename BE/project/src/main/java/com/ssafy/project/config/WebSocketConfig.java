b53b60539fd4ef5562a1af226142e4c1b570ef08
hyeonjin <0208cr@naver.com>
2025-02-03 13:59:51 +0900

    [BE] feat: 웹 소켓 알림 관련 코드 추가
    - WebSocketConfig: 웹 소켓 설정 추가
    - 알림 관련 DTO, Service, ENUM 추가
    
    ref: S12P11D103-149, S12P11D103-183, S12P11D103-184

---
+++ BE/project/src/main/java/com/ssafy/project/config/WebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.ssafy.project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 구독 신청 prefix
+        registry.enableSimpleBroker("/sub");
+        // 메시지 발생 prefix
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+}
