bf824a01f8deacb67c19f8aa2a8c7f6a1a3ee8a6
정은아 <j70529@naver.com>
2025-02-07 13:11:30 +0900

    [BE] fix: 편지 저장, 조회 S3이용하도록 수정
    - put용 presignedUrl과 fileName 생성 api 수정
    - db 저장 api 수정
    - S3에서 조회할 수 있는 get용 presignedUrl이 담긴 dto를 반환하는 api 수정

--- BE/project/src/main/java/com/ssafy/project/service/LetterService.java
+++ BE/project/src/main/java/com/ssafy/project/service/LetterService.java
@@ -4,11 +4,15 @@ import com.ssafy.project.domain.Letter;
 import com.ssafy.project.dto.LetterDto;
 
 import java.util.List;
+import java.util.Map;
 
 public interface LetterService {
-    Letter saveLetter(Long childId, String content, String reply, String bookTitle, String role, String letterRecord);
+    void saveLetter(Long childId, String content, String reply, String bookTitle, String role, String letterFileName);
+
+    Map<String, String> getPresignedUrl();
 
     String getGPTResponse(String fairyTale, String role, String childName, String content);
+
 
     List<LetterDto> getLettersByChildId(Long childId);
 
