21b776104128e53ca2421e2b74f9b35282027c7c
박준현 <wndrjrak12@gmail.com>
2025-02-07 13:00:52 +0900

    [INFRA] chore : 그림그리기 웹소켓 요청 테스트

--- FE/src/components/drawing/hooks/useSocketStore.ts
+++ FE/src/components/drawing/hooks/useSocketStore.ts
@@ -1,63 +1,63 @@
-import { create } from 'zustand';
-import { io, Socket } from 'socket.io-client';
+  import { create } from 'zustand';
+  import { io, Socket } from 'socket.io-client';
 
-export interface drawingData {
-  status: string;
-  color: string;
-  prevX: number;
-  prevY: number;
-  curX: number;
-  curY: number;
-}
+  export interface drawingData {
+    status: string;
+    color: string;
+    prevX: number;
+    prevY: number;
+    curX: number;
+    curY: number;
+  }
 
-interface SocketStore {
-  isConnected: boolean;
-  setIsConnected: (connected: boolean) => void;
-  roomId: string | null;
-  setRoomId: (roomId: string | null) => void;
-  socket: Socket | null;
-  connectSocket: () => void;
-  disconnectSocket: () => void;
-}
+  interface SocketStore {
+    isConnected: boolean;
+    setIsConnected: (connected: boolean) => void;
+    roomId: string | null;
+    setRoomId: (roomId: string | null) => void;
+    socket: Socket | null;
+    connectSocket: () => void;
+    disconnectSocket: () => void;
+  }
 
-const useSocketStore = create<SocketStore>((set, get) => {
-  let socket: Socket | null = null;
+  const useSocketStore = create<SocketStore>((set, get) => {
+    let socket: Socket | null = null;
 
-  return {
-    isConnected: false,
-    setIsConnected: (connected) => {
-      set({ isConnected: connected });
-      if (connected) {
-        get().connectSocket();
-      } else {
-        get().disconnectSocket();
-      }
-    },
+    return {
+      isConnected: false,
+      setIsConnected: (connected) => {
+        set({ isConnected: connected });
+        if (connected) {
+          get().connectSocket();
+        } else {
+          get().disconnectSocket();
+        }
+      },
 
-    roomId: null,
-    setRoomId: (roomId) => set({ roomId }),
+      roomId: null,
+      setRoomId: (roomId) => set({ roomId }),
 
-    socket: null,
+      socket: null,
 
-    connectSocket: () => {
-      if (!socket) {
-        socket = io('https://i12d103.p.ssafy.io/socket.io', { autoConnect: false });
-        socket.connect();
-        socket.on('connect', () => console.log('Connected to socket'));
-        socket.on('disconnect', () => console.log('Disconnected from socket'));
+      connectSocket: () => {
+        if (!socket) {
+          socket = io('wss://i12d103.p.ssafy.io', { autoConnect: false });
+          socket.connect();
+          socket.on('connect', () => console.log('Connected to socket'));
+          socket.on('disconnect', () => console.log('Disconnected from socket'));
 
-        set({ socket });
-      }
-    },
+          set({ socket });
+        }
+      },
 
-    disconnectSocket: () => {
-      if (socket) {
-        socket.disconnect();
-        socket = null;
-        set({ socket: null });
-      }
-    },
-  };
-});
+      disconnectSocket: () => {
+        if (socket) {
+          socket.disconnect();
+          socket = null;
+          set({ socket: null });
+        }
+      },
+    };
+  });
 
-export default useSocketStore;
+  export default useSocketStore;
