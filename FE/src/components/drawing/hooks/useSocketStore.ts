b3fab1df263e5158d968f995af200685383309c5
박준현 <wndrjrak12@gmail.com>
2025-02-07 14:44:16 +0900

    [BE] chore : eslint 띄워쓰기 완화, 그리기 웹소켓 경로 수정

--- FE/src/components/drawing/hooks/useSocketStore.ts
+++ FE/src/components/drawing/hooks/useSocketStore.ts
@@ -42,7 +42,7 @@ const useSocketStore = create<SocketStore>((set, get) => {
     connectSocket: () => {
       if (!socket) {
         // TODO : socket.io 서버 수정
-        socket = io('http://localhost:3869', { autoConnect: false });
+        socket = io('wss://i12d103.p.ssafy.io', { autoConnect: false });
         socket.connect();
         socket.on('connect', () => console.log('Connected to socket'));
         socket.on('disconnect', () => console.log('Disconnected from socket'));
