323b8f635f0c69a188579ca78ddd2a2a233f9adf
박준현 <wndrjrak12@gmail.com>
2025-02-08 20:25:54 +0900

    [INFRA] fix : firebase_config 수정

--- BE/project/src/main/java/com/ssafy/project/firebase/FirebaseConfig.java
+++ BE/project/src/main/java/com/ssafy/project/firebase/FirebaseConfig.java
@@ -3,29 +3,31 @@ package com.ssafy.project.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
+import java.util.Base64;
 
-// Firebase 초기화 설정 클래스
-@Configuration
+@Component
 public class FirebaseConfig {
 
     @PostConstruct
-    public void initalize() {
+    public void initialize() {
         try {
             if (FirebaseApp.getApps().isEmpty()) {
-                ClassPathResource resource = new ClassPathResource("firebase/d103-952ab-firebase-adminsdk-fbsvc-68e87c57be.json");
-                FileInputStream serviceAccount = new FileInputStream(resource.getFile());
+                String firebaseConfigBase64 = System.getenv("FIREBASE_CONFIG");
+
+                // Base64 디코딩 후 Firebase 초기화
+                byte[] decodedBytes = Base64.getDecoder().decode(firebaseConfigBase64);
+                ByteArrayInputStream serviceAccount = new ByteArrayInputStream(decodedBytes);
                 FirebaseOptions options = new FirebaseOptions.Builder()
                         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                         .build();
                 FirebaseApp.initializeApp(options);
             }
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Firebase 초기화 실패", e);
         }
     }
 }
