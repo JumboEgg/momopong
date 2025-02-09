a08ca218f51a2e0576e5488ebbd3a0146a1cb450
박준현 <wndrjrak12@gmail.com>
2025-02-10 04:11:16 +0900

    [BE] feat: 그림 참여기록 토대

---
+++ BE/project/src/main/java/com/ssafy/project/dto/draw/DrawingParticipationDto.java
@@ -0,0 +1,17 @@
+package com.ssafy.project.dto.draw;
+
+import com.ssafy.project.domain.type.ParticipationMode;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class DrawingParticipationDto {
+    private Long drawingParticipationId;  // 참가기록
+    private Long child_id;  // 유저 id
+    private LocalDateTime startTime;  // 시작 시간
+    private LocalDateTime endTime;  // 종료 시간
+    private ParticipationMode mode;  // 참여 모드
+}
