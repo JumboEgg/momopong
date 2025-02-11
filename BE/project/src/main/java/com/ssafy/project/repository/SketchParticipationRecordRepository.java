d0d576f3056478b68fd2a1520919bea63957f7b4
hyeonjin <0208cr@naver.com>
2025-02-12 02:20:51 +0900

    [BE] feat: 리포트 기능 추가
    - 활동 분석 리포트 추가
    - 활동 내역 리포트 추가
    
    ref: S12P11D103-246, S12P11D103-248

--- BE/project/src/main/java/com/ssafy/project/repository/SketchParticipationRecordRepository.java
+++ BE/project/src/main/java/com/ssafy/project/repository/SketchParticipationRecordRepository.java
@@ -1,8 +1,23 @@
 package com.ssafy.project.repository;
 
+import com.ssafy.project.domain.Child;
 import com.ssafy.project.domain.record.SketchParticipationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface SketchParticipationRecordRepository extends JpaRepository<SketchParticipationRecord, Long> {
+    List<SketchParticipationRecord> findAllByChild(Child child);
 
+    // 기간별 시간 조회
+    @Query(value = """
+            SELECT COALESCE(SUM(TIMESTAMPDIFF(MINUTE, start_time, end_time)), 0)
+            FROM sketch_participation_record
+            WHERE child_id = :childId
+            AND start_time between :start and :end
+            """, nativeQuery = true)
+    Long totalBetweenTime(@Param("childId") Long childId, @Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 }
